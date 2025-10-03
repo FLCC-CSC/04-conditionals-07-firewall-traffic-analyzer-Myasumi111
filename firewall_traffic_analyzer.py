5a4d313b17cd833a2c021cd2df342a2d6e6d1ee0
Myasumi111 <myasumi@fingerlakes.edu>
2025-10-03 05:34:42 -0400

    Update firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -24,7 +24,11 @@ def analyzer():
 
     port = int(input("Enter the port number (e.g., 80, 22, 443, 3389): "))
     size = int(input("Enter the data transfer size in megabytes (MB): "))
-    if port == 22 and size > 500:
+    if port == 22 and size == 100:
+        risk = "HIGH RISK: Potential unauthorized remote access detected!"
+    elif port == 3389 and size == 100:
+         risk = "HIGH RISK: Potential unauthorized remote access detected!"       
+    elif port == 22 and size > 500:
         risk = "HIGH RISK: Potential unauthorized remote access detected!"
     elif port == 80 and size > 100:
         risk = "MEDIUM RISK: Large unencrypted data transfer detected."
